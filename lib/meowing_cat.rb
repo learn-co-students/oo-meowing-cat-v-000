c683b07a6a32b7ec701381f62abb3ea1f8dac171
Nicholas-Maxwell <nick.maxwell18@gmail.com>
2016-06-10 14:53:33 -0400

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,12 @@
-## code your solution here. 
+
+
+class Cat #instantiates a new cat 
+
+attr_accessor:name #macro to create a setter and getter method for a cat's name.
+
+def meow
+  puts "meow!"
+
+end 
+
+end 
