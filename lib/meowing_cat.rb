c611f47cb4377cdc5db7701cd815b251651258bc
Karen Berba <kberba@gmail.com>
2017-07-17 01:30:41 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,14 @@
-## code your solution here. 
+## code your solution here.
+#Cat
+#  instantiates a new cat
+#  receives attr_accessor
+#  has a name
+#  is able to meow
+
+class Cat
+  attr_accessor :name
+
+  def meow
+    puts "meow!"
+  end
+end
