37f157de8556da1383e1a4e82c86ebdccfb9aba0
Christopher DiStasio <cadistasio@gmail.com>
2017-10-16 18:49:39 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,19 +1,11 @@
 class Cat
   attr_accessor :name
-  attr_accessor :meow
-  
+  def meow
+  end
 end
 
-def meow
-  @meow
-end
-
-def meow=(meowing_cat)
-   @meow = meowing_cat
-end
-
-specific_meowing_cat = Cat.new
-
-specific_meowing_cat.meow = "meow!"
-
-puts specific_meowing_cat
+# specific_meowing_cat = Cat.new
+#
+# specific_meowing_cat.meow = "meow!"
+#
+# puts specific_meowing_cat
