12884e151431dc253bf9467da2643afaf68f0db1
Charity <Youngblood.charity@gmail.com>
2017-11-09 12:27:35 -0800

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,16 +1,18 @@
 require 'pry'
 ## code your solution here. 
-class Cat 
-  attr_accessor :name
-  
+class Cat   
   def name=(cats_name)
     @name = cats_name
   end
   def name
     @name
   end
- 
+  def meow
+    puts "meow!"   
  end
+  attr_accessor :cats_name
+end
 
 maru = Cat.new
 maru.name = "Maru"
+maru.meow
