d5aa95758635f8198d93ac8e6b743494d056026d
Lekan Nicholson <lekan.nicholson@gmail.com>
2017-12-02 14:49:31 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,8 @@
-## code your solution here. 
+## code your solution here.
+class Cat #Defines Cat class
+  attr_accessor :name #creates the name setter and getter methods
+
+  def meow #creates the meow method that makes the cat class meow
+    puts "meow!"
+  end
+end
