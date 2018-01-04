75f8c78662f0485a8e2e9b4d2e86aad99a07da66
EPIC448 <samrey2018@gmail.com>
2018-01-04 18:22:59 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,28 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+
+  # def initialize(name)
+  #   @name = name
+  #
+  # end
+
+  def name
+  attr_accessor :name
+  end
+
+  def meow
+    attr_accessor :meow
+
+  end
+end
+
+
+maru = Cat.new
+maru.name = "Maru"
+
+maru.name
+# => "Maru"
+
+maru.meow
+# "meow!"
+# => nil
