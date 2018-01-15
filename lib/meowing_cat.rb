3b99ae10b74051b0efa714fdb9a56190e087c3ad
Matthew Crooks <mfcrooks@gmail.com>
2018-01-15 22:59:05 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,12 @@
-## code your solution here. 
+# class Person
+#   attr_accessor :name
+#   attr_writer :name
+#   attr_reader :name
+# end
+## code your solution here.
+class Cat
+  attr_accessor :name
+  def meow
+    puts "meow!"
+  end
+end
