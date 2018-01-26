c31b62da97270fd2cdf39db04f51888831f4d34c
Erica Murdock-Waters <ericahpa@gmail.com>
2018-01-26 07:30:15 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -2,7 +2,7 @@
 class Cat                # Class Cat instantiates a new cat
   attr_accessor :name    # Receives attr_accessor and has a name
 
-  def meow
+  def meow               # Define a method that allows any instance of Cat to meow
     puts "meow!"
   end
 
