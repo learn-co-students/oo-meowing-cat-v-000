301004d9ec5ec4260cf0588b6fef2d8537a077fc
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-03-31 00:56:33 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,5 +1,6 @@
 class Cat
-  # Ruby weirdness - :name here but if used in method, it would be @name
+  # Ruby weirdness - :name here but if used in method, it would be @name.
+  # So attr_accessor needs the symbol and that corresponds to the instance variable name...
   attr_accessor :name
   def meow
     puts "meow!"
