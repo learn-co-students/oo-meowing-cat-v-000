56e9d4f3c0f03ab2f65657e09b02bf8dd3fe6138
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-20 02:51:58 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,27 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+
+  attr_accessor :name
+
+  def meow
+    puts "meow!"
+  end
+
+end
+
+
+  #attr_accessor is really two macros attr_writer, and attr_reader.
+  #a getter and a setter.
+
+  #attr_accesor is the easier way of doing getters and setters all in one.
+#no more...
+ 
+  #def name=(name)
+  #  @name = name
+  #end
+  # or 
+  #def name
+  #  @name
+  #end
+  
+
