a49e848a45430649810c609d177cd2e44894dd9e
Lois Lee <lois.lee@me.com>
2018-10-24 23:44:35 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,16 +1,8 @@
 class Cat
-
-  attr_accessor :cat_name
-
-  def name=(cat_name)
-    @this_cats_name = cat_name
-  end
-
-  def name
-    @this_cats_name
-  end
+  attr_accessor:name
 
   def meow
     puts "meow!"
   end
+
 end
