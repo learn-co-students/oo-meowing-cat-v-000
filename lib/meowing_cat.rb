2ad43b7360e1664190e656fee5ea6a18a9fb2bfa
Toni-Ty <antonique2479@yahoo.com>
2019-03-02 02:03:35 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,19 @@
-## code your solution here. 
+class Cat
+  attr_accessor :name
+  attr_accessor :meow
+
+end
+
+def name
+  @name
+end
+
+def meow
+  @meow
+end
+
+
+cat = Cat.new
+cat = cat.name
+cat = cat.meow
+cat.meow
