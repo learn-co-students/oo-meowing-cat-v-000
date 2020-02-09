52604e541e42ac5fe781f276c916bb7798a01a09
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-02-09 08:30:04 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,13 @@
-## code your solution here. 
+class Cat 
+  attr_accessor :name
+  
+  def meow
+   @name 
+   puts "meow!"
+   end
+ end
+ 
+ muana = Cat.new 
+ muana.name = "Muana"
+ muana.name
+ muana.meow
